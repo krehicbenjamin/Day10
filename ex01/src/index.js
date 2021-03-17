959dfd98649a0ea64fe80d33d32749546182fb16
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-17 21:46:38 +0100

    adding the ex01 folder

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+var myPet = {
+  species: "Dog",
+  name: "Bob",
+  legs: 4,
+  friends: ["Jimmy", "Bed"],
+};
+function myFucntion(myObj) {
+  return myOb;
+}
+console.log(myFucntion(myPet));
+module.exports = { myPet, myFucntion };
